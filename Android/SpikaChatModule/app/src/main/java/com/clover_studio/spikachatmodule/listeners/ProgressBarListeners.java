82fd2e626b5be028fa523aa24118a3e98ac1e77d
VarPDev <pako96pako@gmail.com>
2016-04-25 00:22:20 +0200

    vuoto

--- Android/SpikaChatModule/app/src/main/java/com/clover_studio/spikachatmodule/listeners/ProgressBarListeners.java
+++
@@ -1,7 +0,0 @@
-package com.clover_studio.spikachatmodule.listeners;
-
-public interface ProgressBarListeners {
-	public void onSetMax(long total);
-	public void onProgress(long current);
-	public void onFinish();
-}
